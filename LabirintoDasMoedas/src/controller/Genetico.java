461c62deb870f0a72debddb94d06d182f683f639
Luiza Pereira <luiza.pereira.001@acad.pucrs.br>
2018-10-03 21:00:11 -0300

    Aptdar com a média dos valores dos sacos

--- LabirintoDasMoedas/src/controller/Genetico.java
+++ LabirintoDasMoedas/src/controller/Genetico.java
@@ -1,12 +1,21 @@
 package controller;
 
+import java.util.List;
 import java.util.Random;
+
+import model.SacoDeMoedas;
 
 public class Genetico {
 
+	static int[] carga = {40,50,70,40,60,70,40,50,60,20,40,50,60,30,40,50,10};
 	
-	static int[] carga = {};
-	
+	static int bau0 = 0;
+	static int bau1 = 0;
+	static int bau2 = 0;
+	static int bau3 = 0;
+	static int total = 0;
+ 	
+
 	public static void main(String[] args) {
 		int [][]populacao = new int[5][17];
 		int [][]intermediaria = new int[5][17];
@@ -16,7 +25,7 @@ public class Genetico {
 		for (int i=0;i<5;i++) {
 			System.out.println("Geracao " + i);
 			aptidar(populacao);
-			printPopulacao(populacao,17);
+			printPopulacao(populacao,16);
 	
 			elitizar(populacao, intermediaria);
 
@@ -24,16 +33,22 @@ public class Genetico {
 		}
 	}
 	
+	private static void iniciarCarga(List<SacoDeMoedas> sacosDeMoedas) {
+		
+		// PARA PEGAR OS VALORES DO SACO DE MOEDA E GERAR A CARGA
+		
+	}
+
 	static void clonar(int [][]destino, int [][]origem) {
 		for (int i=0; i<5; i++) {
-			for (int j=0; j<11; j++) {
+			for (int j=0; j<16; j++) {
 				destino[i][j] = origem[i][j];
 			}
 		}
 	}
 	
 	static void clonar(int []destino, int []origem) {
-		for (int j=0; j<11; j++) {
+		for (int j=0; j<16; j++) {
 			destino[j] = origem[j];
 		}
 	}
@@ -48,7 +63,7 @@ public class Genetico {
 				intermediaria[linha][j] = populacao[pai][j];
 				intermediaria[linha+1][j] = populacao[mae][j];
 			}
-			for (int j=5; j<17; j++) {
+			for (int j=5; j<16; j++) {
 				intermediaria[linha][j] = populacao[mae][j];
 				intermediaria[linha+1][j] = populacao[pai][j];
 			}
@@ -61,13 +76,15 @@ public class Genetico {
 		Random r = new Random();
 		int primeiro = r.nextInt(5);
 		int segundo = r.nextInt(5);
-		return (populacao[primeiro][17] < populacao[segundo][17]) ? primeiro : segundo;
+		return (populacao[primeiro][16] < populacao[segundo][16]) ? primeiro : segundo;
+		
+		//fazer o torneio, pegar metade e mudar os pares por impares e os impares por pares
 	}
 	
 	static void elitizar(int [][]populacao, int [][]intermediaria) {
 		int indexMenor = 0;
 		for (int i=0; i<5; i++) {
-			if (populacao[i][17] < populacao[indexMenor][17]) {
+			if (populacao[i][16] < populacao[indexMenor][16]) {
 				indexMenor = i;
 			}
 		}
@@ -76,18 +93,56 @@ public class Genetico {
 	
 	static void aptidar(int [][]populacao) {
 		for (int i=0; i<5; i++) {
-			for (int j=0; j<17; j++) {
-				populacao[i][17] += (populacao[i][j]==1) ? carga[j] : -carga[j] ;
+			populacao[i][16]=0;
+			int cont = 0;
+			for (int j=0; j<16; j++) {
+				if(populacao[i][j] == 0)
+				{
+					bau0 = carga[j];
+				}
+				if(populacao[i][j] == 0)
+				{
+					bau1 = carga[j];
+				}
+				if(populacao[i][j] == 0)
+				{
+					bau2 = carga[j];
+				}
+				if(populacao[i][j] == 0)
+				{
+					bau3 = carga[j];
+				}
+			
 			}
-			populacao[i][17] = Math.abs(populacao[i][17]);
+			
+			total = bau0 + bau1 + bau2 + bau3;
+			
+			if(total/4 == bau0)
+			{
+				cont++;			
+			}
+			if(total/4 == bau0)
+			{
+				cont++;			
+			}
+			if(total/4 == bau0)
+			{
+				cont++;			
+			}
+			if(total/4 == bau0)
+			{
+				cont++;			
+			}
+			
+			populacao[i][16] = cont;
 		}
 	}
 
 	static void popular(int [][]populacao) {
 		Random r = new Random();
 		for (int i=0; i<5; i++)
-			for (int j=0; j<17; j++) {
-				populacao[i][j] = r.nextInt(2);
+			for (int j=0; j<16; j++) {
+				populacao[i][j] = r.nextInt(4); 
 			}
 	}
 	
